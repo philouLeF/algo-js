576899e416b74799a5354eeda487e8caf1da331f
philouLeF <delcroixphilippe73@gmail.com>
2022-02-18 14:01:14 +0100

    3.1

---
+++ 3.arrays/3.1.js
@@ -0,0 +1,20 @@
+let arr1 = [1, 2, 3, 4, 5];
+let sum1 = 0;
+
+for(i = 0; i < arr1.length; i++){
+    sum1 += arr1[i];
+}
+
+console.log(sum1);
+
+
+let arr2 = [100, 101, 102];
+let sum2 = 0;
+
+
+for(j = 0; j < arr2.length; j++){
+    sum2 += arr2[j];
+}
+
+
+console.log(sum2);
